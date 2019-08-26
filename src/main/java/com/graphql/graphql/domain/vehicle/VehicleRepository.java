11293220fe081e6f53012c8b406b0ce2e3f41a3a
frfontoura <frfontoura@gmail.com>
2019-08-25 22:13:33 -0300

    Criação dos resolvers/mutations de vehicles

---
+++ src/main/java/com/graphql/graphql/domain/vehicle/VehicleRepository.java
@@ -0,0 +1,8 @@
+package com.graphql.graphql.domain.vehicle;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
+}
